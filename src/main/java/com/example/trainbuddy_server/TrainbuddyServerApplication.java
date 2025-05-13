66fda912027597eecb358ffea85230510e5251ee
diedasilva <dasilva.diego@outlook.fr>
2025-05-13 19:04:19 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/trainbuddy_server/TrainbuddyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainbuddy_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainbuddyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainbuddyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trainbuddy_server/TrainbuddyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainbuddy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainbuddyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
